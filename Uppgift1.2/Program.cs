84430f2f9a6d5cff711d071f1c585f92dbaa4f7c
majo06005 <majo06005@E-PW0234K2>
2023-09-06 10:36:13 +0200

    Add project files.

---
+++ Uppgift1.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Uppgift_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;//Text färgen.
+            Console.BackgroundColor = ConsoleColor.DarkBlue;//Backrundsfärgen.
+            Console.Clear();//Gör så hela backgrunden blir blå.
+            Console.WriteLine("!!VIRUS DECTECTED ON DEVICE!!");//Virus
+            Console.ReadKey();
+        }
+    }
+}
